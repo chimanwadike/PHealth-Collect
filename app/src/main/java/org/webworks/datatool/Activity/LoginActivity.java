382dff68c5cb7ee27389bf655046ca4dbbefac42
CNwadike <chimanwadike94@gmail.com>
2020-03-02 23:17:21 +0100

    implemented app login design

---
+++ app/src/main/java/org/webworks/datatool/Activity/LoginActivity.java
@@ -0,0 +1,26 @@
+package org.webworks.datatool.Activity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import org.webworks.datatool.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
